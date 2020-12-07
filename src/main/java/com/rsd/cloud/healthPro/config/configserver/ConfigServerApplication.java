ac0cace24959ac92b9d8ce49d78c1bcf068a6f77
zhang.liangliang <zhangliangliang@ouyada.com>
2020-12-07 18:30:07 +0800

    debug

--- src/main/java/com/rsd/cloud/healthPro/config/configserver/ConfigServerApplication.java
+++ src/main/java/com/rsd/cloud/healthPro/config/configserver/ConfigServerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
